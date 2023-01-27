dbcf644cc76eed45eca3a020e86fa3c214673c67
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-27 17:54:26 +0700

    test pypi

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import gempa_terkini_BMKG
 if __name__ == "__main__":
     print("Aplikasi utama")
     result = gempa_terkini_BMKG.ekstraski_data()
-    gempa_terkini_BMKG(result)
-
+    gempa_terkini_BMKG.tampilkan_data(result)
